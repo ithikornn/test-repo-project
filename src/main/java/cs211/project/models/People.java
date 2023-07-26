f5b188879db0c5148926604f441856509a3af027
tent <ithikorn.u@ku.th>
2023-07-26 11:38:33 +0700

    attributes test

--- src/main/java/cs211/project/models/People.java
+++ src/main/java/cs211/project/models/People.java
@@ -2,7 +2,10 @@ package cs211.project.models;
 
 public class People {
 
+    private String nane;
+    private int money;
     public People(String name , int money) {
-
+        this.nane = name;
+        this.money = money;
     }
 }
